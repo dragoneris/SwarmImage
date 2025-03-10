e156ef17d5cedfd628e36ae2b87d3399b4d421cd
Eris <eris@dragoneris.com>
2025-03-10 12:44:30 -0400

    Autolaunch Swarm after installing ComfyUI Manager

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ if [ -d "SwarmUI" ]; then
 		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru.csv -o SwarmUI/Data/Autocompletions/danbooru.csv
 		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/e621.csv -o SwarmUI/Data/Autocompletions/e621.csv
 		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru_e621_merged.csv -o SwarmUI/Data/Autocompletions/danbooru_e621_merged.csv
+
+		./SwarmUI/launch-linux.sh --launch_mode none --host 0.0.0.0
 	fi
 else
 	./install-linux.sh --launch_mode none --host 0.0.0.0
