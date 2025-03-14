6d0111c197f5d436593076c3f693535d236f9d49
Eris <eris@dragoneris.com>
2025-03-14 12:33:45 -0400

    Wget is not cURL

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ if [ -d "SwarmUI" ]; then
 		git clone https://github.com/ltdrdata/ComfyUI-Manager/ "SwarmUI/dlbackend/ComfyUI/custom_nodes/ComfyUI-Manager"
 
 		# Grab autocomplete files
-		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru.csv -o SwarmUI/Data/Autocompletions/danbooru.csv
-		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/e621.csv -o SwarmUI/Data/Autocompletions/e621.csv
-		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru_e621_merged.csv -o SwarmUI/Data/Autocompletions/danbooru_e621_merged.csv
+		wget -O SwarmUI/Data/Autocompletions/danbooru.csv https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru.csv
+		wget -O SwarmUI/Data/Autocompletions/e621.csv https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/e621.csv 
+		wget -O SwarmUI/Data/Autocompletions/danbooru_e621_merged.csv https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru_e621_merged.csv 
 
 		./SwarmUI/launch-linux.sh --launch_mode none --host 0.0.0.0
 	fi
